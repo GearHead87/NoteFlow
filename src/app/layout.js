6e856a4740de66deaf743c0a44d1f0c74b1c2646
GearHead87 <hosanulislam87@gmail.com>
2024-06-19 13:41:22 +0600

    navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,30 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/lib/theme-provider";
+import Navbar from "@/components/shared/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NoteFlow",
-  description: "Your seamless solution for organized and efficient note-taking.",
+	title: "NoteFlow",
+	description:
+		"Your seamless solution for organized and efficient note-taking.",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	NavigationMenuViewport,
+	navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import Link from "next/link";
+
+const Navbar = () => {
+	return (
+		<div className="flex justify-center items-center">
+			<NavigationMenu>
+				<NavigationMenuList>
+					<NavigationMenuItem>
+						<Link href="/" legacyBehavior passHref>
+							<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+								Home
+							</NavigationMenuLink>
+						</Link>
+					</NavigationMenuItem>
+
+					<NavigationMenuItem>
+						<NavigationMenuTrigger>Item two</NavigationMenuTrigger>
+						<NavigationMenuContent>
+							<NavigationMenuLink>Link</NavigationMenuLink>
+						</NavigationMenuContent>
+					</NavigationMenuItem>
+				</NavigationMenuList>
+			</NavigationMenu>
+		</div>
+	);
+};
+
+export default Navbar;
