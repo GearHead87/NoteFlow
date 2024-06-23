755a9919f3fdcf8071da3b529c07c837063845c3
GearHead87 <hosanulislam87@gmail.com>
2024-06-23 23:18:19 +0600

    login and signup page extra link added:

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,17 @@
 import LoginForm from "@/components/Form/LoginForm";
+import Link from "next/link";
 import React from "react";
 
 const LoginPage = () => {
 	return (
 		<div>
 			<LoginForm />
+			<h6 className="my-12 text-center">
+				Not Have Account?{" "}
+				<Link className="text-primary font-semibold" href={"/signup"}>
+					Sign Up
+				</Link>
+			</h6>
 		</div>
 	);
 };
--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,9 +1,16 @@
 import SignupForm from "@/components/Form/SignupForm";
+import Link from "next/link";
 
 const signupPage = () => {
 	return (
 		<div>
 			<SignupForm />
+			<h6 className="my-12 text-center">
+				Already Have Account?{" "}
+				<Link className="text-primary font-semibold" href={"/login"}>
+					Login
+				</Link>
+			</h6>
 		</div>
 	);
 };
