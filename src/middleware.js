a41aecff46fa257be76c77ea2a80b6eaf699acce
GearHead87 <hosanulislam87@gmail.com>
2024-06-24 00:06:10 +0600

    middleware fixed

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,21 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
+const getToken = (request) => {
+	// Try to get the secure token first
+	let token = cookies(request).get("__Secure-next-auth.session-token");
+
+	// If the secure token is not found, try the non-secure token
+	if (!token) {
+		token = cookies(request).get("next-auth.session-token");
+	}
+
+	return token;
+};
+
 export const middleware = async (request) => {
-	// const token = cookies(request).get("__Secure-next-auth.session-token");
-	const token = cookies(request).get("next-auth.session-token");
+	const token = getToken(request);
+
 	const pathname = request.nextUrl.pathname;
 	if (pathname.includes("api")) {
 		return NextResponse.next();
